f33f530b02fb33756677af2eab2a485ee713a97f
felipe <felipeaugustodluz@hotmail.com>
2023-03-16 20:42:26 -0300

    Main commented. No conflict!!!

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,22 @@
+import enums.Compability;
+import enums.MouseType;
+
 import static java.lang.Thread.sleep;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Processor cpu = new Processor(1050, 1,"gray", true);
+//        Processor cpu = new Processor(1050, 1,"gray", true);
+//        cpu.Show();
+//        cpu.Functionality();
+//
+//        Headset headset = new Headset(333, 1, "RED", true, "Razor","Abyssus", Compability.xbox);
+//        headset.Show();
+//
+//        Mouse mouse = new Mouse(150, 1 , "BLACK", false, "Redragon","Kraken", MouseType.wireless);
+//        mouse.Show();
 
-        cpu.Show();
-        cpu.Functionality();
 
     }
 }
